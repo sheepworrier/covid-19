8c7fef0bfdea2e00c20490355c13bfe3078c0194
Dean Perry <djp42@Deans-iMac.cable.virginm.net>
2020-11-01 23:03:10 +0000

    Replace legends with labels

--- msoa_data.R
+++ msoa_data.R
@@ -1,31 +1,43 @@
 library(readr)
 library(ggplot2)
-library(plotly)
+library(ggrepel)
 
 msoa_data <- read_csv(paste0("https://coronavirus-staging.data.gov.uk/",
                              "downloads/msoa_data/MSOAs_latest.csv"))
 
+msoas_of_interest <-
+  data.frame(areaName = c("North Laine & the Lanes", "Aldrington South",
+                          "Hassocks, Keymer & East Hurstpierpoint",
+                          "Bradley", "World's End",
+                          "Talbot & Branksome Woods"),
+             who = c("Dean & Julie", "Neil", "Andrea", "Shirley", "Gwenda",
+                     "Russell")
+  )
+
 filtered_data <- msoa_data %>%
-  filter(areaName %in% c("North Laine & the Lanes", "Aldrington South",
-                         "Hassocks, Keymer & East Hurstpierpoint",
-                         "Bradley", "World's End",
-                         "Talbot & Branksome Woods"),
-         date >= max(msoa_data$date, na.rm = TRUE) - 90) %>%
+  inner_join(msoas_of_interest) %>%
+  filter(date >= max(msoa_data$date, na.rm = TRUE) - 90) %>%
   mutate(newCasesBySpecimenDateRollingRate =
            if_else(is.na(newCasesBySpecimenDateRollingRate),
                    2,
-                   newCasesBySpecimenDateRollingRate))
+                   newCasesBySpecimenDateRollingRate),
+         label = paste(who, areaName,
+                       round(newCasesBySpecimenDateRollingRate, digits = 0),
+                       sep = " - "))
 
 p <- ggplot(filtered_data) +
   geom_line(aes(x = date, y = newCasesBySpecimenDateRollingRate,
-                group = areaName, colour = areaName)) +
+                group = who, colour = who)) +
+  geom_label_repel(data = filtered_data %>%
+               filter(date == max(msoa_data$date, na.rm = TRUE)),
+             aes(label = label, x = date,
+                 y = newCasesBySpecimenDateRollingRate)) +
   xlab("Date") +
   ylab("Number of positive tests in previous 7 days per 100k") +
   ggtitle(paste0("The number of cases in last 7 days per 100k by MSOA.  ",
                  "Last updated: ", max(filtered_data$date))) +
   scale_x_date(date_breaks = "2 weeks", date_labels = "%b %d") +
-  labs(caption = "Source: http://coronavirus.data.gov.uk")
-
-ggplotly(p)
+  labs(caption = "Source: http://coronavirus.data.gov.uk") +
+  theme(legend.position = "none")
 
 p + ggsave("UK MSOAs.png", height = 12, width = 24, units = "cm")
--- plotly_chart_uk_ltlas.R
+++ plotly_chart_uk_ltlas.R
@@ -1,29 +1,43 @@
 library(readr)
 library(ggplot2)
-library(plotly)
+library(ggrepel)
 
 all_data <-
   read_csv("https://www.dropbox.com/s/nai0vhgr3tlcfb8/uk_rate_data.csv?dl=1")
 
 latest_data <- max(all_data$date, na.rm = FALSE)
 
+ltlas_of_interest <-
+  data.frame(areaName = c("Brighton and Hove", "United Kingdom",
+                          "Wolverhampton", "Enfield", "Mid Sussex",
+                          "Bournemouth, Christchurch and Poole"),
+             who = c("Dean, Julie & Neil", "UK", "Shirley", "Gwenda", "Andrea",
+                     "Russell")
+  )
+
 filtered_data <- all_data %>%
-  filter(areaName %in% c("Brighton and Hove", "United Kingdom",
-                         "Wolverhampton", "Enfield", "Mid Sussex",
-                         "Bournemouth, Christchurch and Poole"),
-         date <= latest_data - 4,
-         date >= latest_data - 90)
+  inner_join(ltlas_of_interest) %>%
+  filter(date <= latest_data - 4,
+         date >= latest_data - 90) %>%
+  mutate(label = if_else(who == "UK",
+                         paste(who, round(specimen_7_day_rate, digits = 0),
+                               sep = " - "),
+                         paste(who, areaName,
+                               round(specimen_7_day_rate, digits = 0),
+                               sep = " - ")))
 
 p <- ggplot(filtered_data) +
   geom_line(aes(x = date, y = specimen_7_day_rate, group = areaName,
                 colour = areaName)) +
+  geom_label_repel(data = filtered_data %>%
+                     filter(date == latest_data - 4),
+                   aes(label = label, x = date, y = specimen_7_day_rate)) +
   xlab("Date") +
   ylab("Number of positive tests in previous 7 days per 100k") +
   ggtitle(paste0("The number of cases in last 7 days per 100k by LTLA.  ",
                  "Last updated: ", max(filtered_data$date))) +
   scale_x_date(date_breaks = "2 weeks", date_labels = "%b %d") +
-  labs(caption = "Source: http://coronavirus.data.gov.uk")
-
-ggplotly(p)
+  labs(caption = "Source: http://coronavirus.data.gov.uk") +
+  theme(legend.position = "none")
 
 p + ggsave("UK LTLAs.png", height = 12, width = 24, units = "cm")
